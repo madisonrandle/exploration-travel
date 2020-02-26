ec8ad814056b3fb2df4799ec18d6313f0afe5be8
Madison Randle <madisonrandle25@gmail.com>
2020-02-26 15:38:12 -0700

    Import main sass file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+import './css/main.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
