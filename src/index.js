02963ec81bdf16b43d28c904ff1e1aef5ef672b5
Madison Randle <madisonrandle25@gmail.com>
2020-02-26 15:35:42 -0700

    Add fetch syntax

--- src/index.js
+++ src/index.js
@@ -10,4 +10,35 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+//
+
+let here;
+let here;
+let here;
+
+
+const someData = fetch('')
+  .then(someResponse => someResponse.json())
+  .then(someData => someData.here)
+  .catch(error => console.log('someData error'));
+
+const someData = fetch('')
+  .then(someResponse => someResponse.json())
+  .then(someData => someData.here)
+  .catch(error => console.log('someData error'));
+
+const someData = fetch('')
+  .then(someResponse => someResponse.json())
+  .then(someData => someData.here)
+  .catch(error => console.log('someData error'));
+
+Promise.all([])
+  .then(someData => {
+    here = someData[0];
+    here = someData[1];
+    here = someData[2];
+  })
+  .catch(error => {console.log('Something is amiss with promise all', error)});
+
+
+//
