b268409ffa57ec01a4f5fbfaea3b01152efeba56
Madison Randle <madisonrandle25@gmail.com>
2020-02-27 16:33:51 -0700

    Pass through destinationData as an argument in the submit button handler

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import './css/main.scss';
 import domUpdates from './domUpdates';
 
 let userId;
+let travelersData;
+let singleTravelerData;
+let tripsData;
+let destinationsData;
 
 const getTravelersData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
   .then(response => response.json())
-  .then(travelersData => {
-    domUpdates.showLogInForm(travelersData.travelers)
-    $('.submit-user-info').click((e) => submitLoginInHelper(e, travelersData.travelers));
-   // want to know why event listeners ^ wont work in domUpdates??
-  })
+  .then(travelersData => travelersData.travelers)
   .catch(error => console.log(`There was an error: ${error}`));
 
 const getSingleTravelerData = () => {
@@ -26,18 +26,27 @@ const getTripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/
   .then(tripsData => tripsData.trips)
   .catch(error => console.log(`There was an error: ${error}`));
 
-// Promise.all([getTravelersData, getSingleTravelerData, getTripsData])
-//   .then(data => {
-//     travelersData = data[0];
-//     singleTravelerData = data[1];
-//     tripsData = data[2];
-//   })
+const getDestinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
+  .then(response => response.json())
+  .then(destinationsData => destinationsData.destinations)
+  .catch(error => console.log(`There was an error: ${error}`));
 
-const submitLoginInHelper = (e, travelersData) => {
+Promise.all([getTravelersData, getSingleTravelerData, getTripsData, getDestinationsData])
+  .then(data => {
+    travelersData = data[0];
+    singleTravelerData = data[1];
+    tripsData = data[2];
+    destinationsData = data[3];
+  })
+  .then(() => {
+    domUpdates.showLogInForm();
+    $('.submit-user-info').click((e) => submitLoginInHelper(e, travelersData, tripsData, destinationsData));
+  })
+  .catch(error => console.log(`There was an error: ${error}`));
+
+const submitLoginInHelper = (e, travelersData, tripsData, destinationsData) => {
   e.preventDefault();
-  userId = domUpdates.validateUser(e, travelersData);
-  getSingleTravelerData();
-  getTripsData();
+  userId = domUpdates.validateUser(e, travelersData, tripsData, destinationsData);
 }
 
 
