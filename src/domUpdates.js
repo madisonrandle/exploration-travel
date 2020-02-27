b4dc20f785497f0777b3c6bcdf8bd93950a56530
Madison Randle <madisonrandle25@gmail.com>
2020-02-26 19:10:51 -0700

    Create method to validate agency username and password

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
 
+
+
 const domUpdates = {
 
   showLogIn: (travelersData) => {
@@ -9,10 +11,23 @@ const domUpdates = {
         <input class='username' type='text'></input>
         <label>PASSWORD:</label>
         <input class='password' type='password'></input>
+        <button class='submit-user-info'>Plan Adventure!</button>
       </form>
       `)
+  },
+
+  validateUser: (e) => {
+    e.preventDefault()
+    if ($('.username').val() === 'agency' && $('.password').val() === 'travel2020') {
+      // make landing page once user is validated as agency
+    }
   }
+
+  //username: agency
+  // password: travel2020
+
 }
+
 
 export default domUpdates;
 //
