677f23fe257f752446fcf698189fb51528e95edb
Madison Randle <madisonrandle25@gmail.com>
2020-02-26 18:45:42 -0700

    Invoke showLogIn method on fetch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import $ from 'jquery';
 import './css/main.scss';
+import domUpdates from './domUpdates';
 
 const getTravelersData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
   .then(response => response.json())
-  .then(travelersData => console.log(travelersData.travelers))
+  .then(travelersData => domUpdates.showLogIn(travelersData.travelers))
   .catch(error => console.log(`There was an error: ${error}`));
 
 //
