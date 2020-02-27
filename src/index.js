23685ff011c599e3244e72a00f31a737f89bf0ef
Madison Randle <madisonrandle25@gmail.com>
2020-02-26 22:13:24 -0700

    Refactor to DRY up code

--- src/index.js
+++ src/index.js
@@ -5,9 +5,23 @@ import domUpdates from './domUpdates';
 const getTravelersData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
   .then(response => response.json())
   .then(travelersData => {
-    domUpdates.showLogIn(travelersData.travelers)
-    $('.submit-user-info').click(domUpdates.validateUser);  // want to know why this wont work in domUpdates??
+    domUpdates.showLogInForm(travelersData.travelers)
+    $('.submit-user-info').click((e) => formValidationHelper(e, travelersData.travelers));  // want to know why event listeners wont work in domUpdates??
   })
   .catch(error => console.log(`There was an error: ${error}`));
+
+// const submitButtonHandler = (e, travelersData) => {
+//   formValidationHelper(e, travelersData);
+// }
+
+const formValidationHelper = (e, travelersData) => {
+  e.preventDefault();
+  domUpdates.validateUser(e, travelersData);
+}
+
+
+
+
+
 
 //
