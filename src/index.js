1cafd82ecde6b70aeadb8795e95f66b10646a063
Madison Randle <madisonrandle25@gmail.com>
2020-02-26 19:11:21 -0700

    Invoke validateUser method in fetch from domUpdates

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import domUpdates from './domUpdates';
 
 const getTravelersData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
   .then(response => response.json())
-  .then(travelersData => domUpdates.showLogIn(travelersData.travelers))
+  .then(travelersData => {
+    domUpdates.showLogIn(travelersData.travelers)
+    $('.submit-user-info').click(domUpdates.validateUser);  // want to know why this wont work in domUpdates??
+  })
   .catch(error => console.log(`There was an error: ${error}`));
 
 //
