11bef2d622e5edbd2d7f20bfed536f055883f756
Madison Randle <madisonrandle25@gmail.com>
2020-02-28 03:06:10 -0700

    Add methods to calculate the total spent on trips this year

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
 
 let foundTraveler;
+let totalCost;
+
 const domUpdates = {
-  showLogInForm: () => {
+  showLogInForm: () => { //
     $('.content').html(`
       <form>
         <label>USERNAME:</label>
@@ -12,45 +14,89 @@ const domUpdates = {
         <button class='submit-user-info'>Plan Adventure!</button>
       </form>
       <span class='invalidLoginMessage'></span>
-      `)
+    `)
   },
 
+  // 1
   validateUser: (e, travelersData, tripsData, destinationsData) => {
     foundTraveler = travelersData.find(user => {
       return $('.username').val() === `traveler${user.id}` && $('.password').val() === 'travel2020'
-    })
-
+    });
     if (foundTraveler) {
       domUpdates.showTravelerAccessPage(tripsData, destinationsData);
       return foundTraveler.id;
-    } else if ($('.username').val() === 'agency' && $('.password').val() === 'travel2020') {
-      domUpdates.showAgencyAccessPage(tripsData);
-    } else {
-      domUpdates.showErrorMessage();
     }
+    $('.username').val() === 'agency' && $('.password').val() === 'travel2020' ? domUpdates.showAgencyAccessPage(tripsData) : domUpdates.showErrorMessage();
   },
 
+  // 2.aAccess
   showAgencyAccessPage: (tripsData) => {
     $('.content').html(`
       <h1>Welcome, Boss!</h1>
     `)
   },
 
+  // 2.tAccess
   showTravelerAccessPage: (tripsData, destinationsData) => {
+    domUpdates.getYearlyCostOfTrips(tripsData, destinationsData);
+    let allTrips = domUpdates.getTravelerDesitinationsInfo(tripsData, destinationsData);
+    totalCost = totalCost.toLocaleString("en-US", {style:"currency", currency:"USD"});
     $('.content').html(`
       <h1>Welcome, ${foundTraveler.name}!</h1>
+      <h2>Your Adventures:</h2>
+      <ul class='list'></ul>
+      <p class='total-cost'>You've spent ${totalCost} on trips this year.</p>
     `)
-    domUpdates.getAllTrips(tripsData, destinationsData);
+    allTrips.map(destination => {
+      $('.list').append(`<li>${destination.destination}</li>`);
+    });
   },
 
+  // 3.tAccess
+  getTravelerDesitinationsInfo: (tripsData, destinationsData) => {
+    return domUpdates.getAllTrips(tripsData, destinationsData).reduce((prices, travelDestination) => {
+      prices.push({destination: `${travelDestination.destination}`, lodgingCost: `${travelDestination.estimatedLodgingCostPerDay}`, flightCost: `${travelDestination.estimatedFlightCostPerPerson}`})
+      return prices;
+    }, []);
+  },
+
+  // 4.tAccess
   getAllTrips: (tripsData, destinationsData) => {
-    return destinationsData.reduce((destinations, destination) => {
-      tripsData.forEach(trip => {
+    let destinations = destinationsData.reduce((trips, destination) => {
+      tripsData.find(trip => {
         if (foundTraveler.id === trip.userID && destination.id === trip.destinationID) {
-          destinations.push(destination)
+          trips.push(destination)
         }
       });
-      return destinations;
+      return trips;
+    }, []);
+    domUpdates.getYearlyCostOfTrips(tripsData, destinationsData);
+    return destinations
+  },
+
+  // 5.tAccess
+  getYearlyCostOfTrips: (tripsData, destinationsData) => {
+    totalCost = domUpdates.getCostInfo(tripsData, destinationsData).reduce((yearlyCost, costInfo) => {
+      if (costInfo.date.includes('2020/')) {
+        let tax = costInfo.cost * .10;
+        yearlyCost += costInfo.cost + tax;
+      }
+      return yearlyCost;
+    }, 0);
+    return totalCost;
+  },
+
+  // 6.tAccess
+  getCostInfo: (tripsData, destinationsData) => {
+    return destinationsData.reduce((tripsCosts, destination) => {
+      tripsData.reduce((cost, trip) => {
+        if (foundTraveler.id === trip.userID && destination.id === trip.destinationID) {
+          cost += trip.duration * destination.estimatedLodgingCostPerDay;
+          tripsCosts.push({cost: cost, date: trip.date})
+        }
+        return cost;
+      }, 0);
+      return tripsCosts;
     }, []);
   },
 
@@ -58,7 +104,6 @@ const domUpdates = {
     $('.invalidLoginMessage').text('Invalid Username or Password');
   },
 }
-
 
 export default domUpdates;
 //
