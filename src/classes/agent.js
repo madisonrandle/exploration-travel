0a4b469bc16d464476bbcf6f26f0a852f84ef0ba
Madison Randle <madisonrandle25@gmail.com>
2020-02-29 10:21:08 -0700

    Write method to get pending trip requests

--- src/classes/agent.js
+++ src/classes/agent.js
@@ -3,12 +3,22 @@ import User from './user';
 class Agent extends User {
   constructor(travelers, trips, destinations, today) {
     super(travelers, trips, destinations, today);
-
   };
 
-  yuh() {
-    console.log(this);
-  }
+  getPendingTripRequests() {
+    let finalObj;
+    let tripRequests = this.trips.filter(trip => trip.status === 'pending');
+    return tripRequests.map(trip => {
+      finalObj = { name: '', date: tripRequests.date, numTrav: 0, destination: '' };
+      let name = this.travelers.find(traveler => trip.userID === traveler.id);
+      let destination = this.destinations.find(destination => trip.destinationID === destination.id);
+      finalObj.date = trip.date;
+      finalObj.numTrav = trip.travelers;
+      finalObj.name = name.name;
+      finalObj.destination = destination.destination;
+      return finalObj;
+    });
+  };
 }
 
 export default Agent;
