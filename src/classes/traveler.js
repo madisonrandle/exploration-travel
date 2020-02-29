eb76a67a7d2daa29e2dcb9ecdb977560a3ae3e3f
Madison Randle <madisonrandle25@gmail.com>
2020-02-29 11:42:26 -0700

    Refactor with find prototype

--- src/classes/traveler.js
+++ src/classes/traveler.js
@@ -9,10 +9,13 @@ class Traveler extends User {
   };
 
   getMyTrips() {
-    return this.destinations.reduce((trips, destination) => {
-      this.trips.find(trip => (this.id === trip.userID && destination.id === trip.destinationID) && trips.push(destination));
-      return trips;
-    }, []);
+    return this.getTripsThisYear().filter(trip => {
+      return this.destinations.find(destination => destination.id === trip.destinationID && this.id === trip.userID);
+    });
+    // return this.destinations.reduce((trips, destination) => {
+    //   this.getTripsThisYear().find(trip => (this.id === trip.userID && destination.id === trip.destinationID) && trips.push(destination));
+    //   return trips;
+    // }, []);
   };
 
   getYearlyTripExpenses() {
@@ -23,22 +26,26 @@ class Traveler extends User {
         tripDestinations.forEach(destination => {
           finalObj.cost += (trip.duration * destination.estimatedLodgingCostPerDay) + (trip.travelers * destination.estimatedFlightCostPerPerson)
       });
-      return finalObj
-    })
+      return finalObj;
+    });
   };
 
   // should get the last year from todays date BUT!! ... It's a leap year -_-
   calculateyearlyTripExpenses() {
+
     let yearlyTripExpenses = this.getYearlyTripExpenses().reduce((acc, el) => {
+
       if (el.date.includes('2020/')) {
         let tax = el.cost * .10;
         acc += el.cost + tax;
       }
+
       return acc;
     }, 0);
     yearlyTripExpenses /= 100;
     yearlyTripExpenses = yearlyTripExpenses.toLocaleString("en-US", {style:"currency", currency:"USD"});
     return yearlyTripExpenses;
+
   };
 }
 
