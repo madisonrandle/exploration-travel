0808e2d4a0d3a291e72acf78a69547f6f5f7fa9f
Madison Randle <madisonrandle25@gmail.com>
2020-02-29 03:10:38 -0700

    Refactor domUpdate functions into Travelor class methods

--- src/classes/user.js
+++ src/classes/user.js
@@ -1,27 +1,35 @@
 import $ from 'jquery';
 import domUpdates from '../domUpdates';
-import moment from 'moment';
 
+let traveler;
 let foundTraveler;
 
 class User {
-  constructor (user) {
-    this.travelers = user.getTravelers;
-    this.trips = user.getTrips;
-    this.destinations = user.getDestinations;
+  constructor (travelers, trips, destinations, today) {
+    this.travelers = travelers;
+    this.trips = trips;
+    this.destinations = destinations;
+    this.today = today;
   };
 
   validateUser(e) {
+    e.preventDefault();
     foundTraveler = this.travelers.find(traveler => {
       return $('.username').val() === `traveler${traveler.id}` && $('.password').val() === 'travel2020'
     });
     if (foundTraveler) {
-      domUpdates.showTravelerAccessPage(foundTraveler);
+
+      domUpdates.getTravelerAccess(this.travelers, this.trips, this.destinations, foundTraveler);
+
       return foundTraveler.id;
     } else {
       $('.username').val() === 'agency' && $('.password').val() === 'travel2020' ? domUpdates.getAgentAccess() : domUpdates.showErrorMessage();
     }
   };
+
+
+
+
 }
 
 export default User;
