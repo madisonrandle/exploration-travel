d45458f60a3d04452cf7b577a3dd8aa84fc19766
Madison Randle <madisonrandle25@gmail.com>
2020-02-29 03:14:40 -0700

    Remove unecessary instantiations and functions to instantiate and refactor on appropriate classes

--- src/index.js
+++ src/index.js
@@ -1,53 +1,49 @@
 import $ from 'jquery';
 import './css/main.scss';
 import domUpdates from './domUpdates';
+import User from './classes/user';
+import moment from 'moment';
 
-let userId;
-let travelersData;
-let singleTravelerData;
-let tripsData;
-let destinationsData;
+let today;
+let user;
+let id;
+
+// console.log('timskiez', moment()._d);
 
 const getTravelersData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
   .then(response => response.json())
-  .then(travelersData => travelersData.travelers)
+  .then(getTravelers => getTravelers.travelers)
   .catch(error => console.log(`There was an error: ${error}`));
 
-const getSingleTravelerData = () => {
-  const url = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers/' + userId;
-  fetch(url)
-    .then(response => response.json())
-    .then(singleTravelerData => singleTravelerData)
-    .catch(error => console.log(`There was an error: ${error}`));
-}
+// const getTravelerData = () => {
+//   const url = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers/' + id;
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(getTraveler => getTraveler)
+//     .catch(error => console.log(`There was an error: ${error}`));
+// }
 
 const getTripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips')
   .then(response => response.json())
-  .then(tripsData => tripsData.trips)
+  .then(getTrips => getTrips.trips)
   .catch(error => console.log(`There was an error: ${error}`));
 
 const getDestinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
   .then(response => response.json())
-  .then(destinationsData => destinationsData.destinations)
+  .then(getDestinations => getDestinations.destinations)
   .catch(error => console.log(`There was an error: ${error}`));
 
-Promise.all([getTravelersData, getSingleTravelerData, getTripsData, getDestinationsData])
+Promise.all([getTravelersData, getTripsData, getDestinationsData])
   .then(data => {
-    travelersData = data[0];
-    singleTravelerData = data[1];
-    tripsData = data[2];
-    destinationsData = data[3];
-  })
-  .then(() => {
-    domUpdates.showLogInForm();
-    $('.submit-user-info').click((e) => submitLoginInHelper(e, travelersData, tripsData, destinationsData));
+    let getTravelers = data[0];
+    let getTrips = data[1];
+    let getDestinations = data[2];
+    user = new User(getTravelers, getTrips, getDestinations, today);
+    domUpdates.showLogInForm(user);
+
   })
   .catch(error => console.log(`There was an error: ${error}`));
 
-const submitLoginInHelper = (e, travelersData, tripsData, destinationsData) => {
-  e.preventDefault();
-  userId = domUpdates.validateUser(e, travelersData, tripsData, destinationsData);
-}
-
-
-//
+// const findID = (e) => {
+//   id = user.validateUser(e);
+// }
